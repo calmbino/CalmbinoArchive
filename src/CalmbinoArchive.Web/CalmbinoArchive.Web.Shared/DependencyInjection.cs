f08286de99481f669bd54003ccef4467aefe0d6b
Calmbino <calmbino@gmail.com>
2024-10-05 12:44:11 +0900

    add httpClient in WebShared

--- src/CalmbinoArchive.Web/CalmbinoArchive.Web.Shared/DependencyInjection.cs
+++ src/CalmbinoArchive.Web/CalmbinoArchive.Web.Shared/DependencyInjection.cs
@@ -6,6 +6,9 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddWebShared(this IServiceCollection services)
     {
+        services.AddHttpClient("CalmbinoArchiveAPI",
+            client => client.BaseAddress = new Uri("https://localhost:7234"));
+
         return services;
     }
 }
