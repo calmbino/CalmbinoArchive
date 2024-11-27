6d38b87fa1512226ac9b6b7b913da14197e251d6
Calmbino <calmbino@gmail.com>
2024-11-27 23:01:27 +0900

    Web.Shared 프로젝트에 DI 관련 라이브러리 추가 및 세팅

---
+++ src/CalmbinoArchive.Web/CalmbinoArchive.Web.Shared/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,11 @@
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CalmbinoArchive.Web.Shared.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddWebShared(this IServiceCollection services)
+    {
+        return services;
+    }
+}
