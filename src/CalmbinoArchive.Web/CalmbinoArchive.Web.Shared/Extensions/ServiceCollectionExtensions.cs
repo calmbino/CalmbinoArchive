b46c4585a5ffc47236a93df64d5e98cb6247b1c7
Calmbino <calmbino@gmail.com>
2024-12-04 22:41:33 +0900

    markdown editor  및 html 렌더링 테스트
    - 마크다운 편집기는 erossini/BlazorMarkdownEditor 패키지를 사용
      - Blazor에 적용하기에 가장 적합
      - 현재 code highlighting은 동작하지 않지만 곧 해결될 것으로 보임
    - 마크다운을 html로 변환 후 렌더링
      - 기본적으로 `MarkupString`을 사용하는 방법이 있지만,
        Mudblazor.Markdown 패키지를 이용
      - Mudblazor의 UI/UX 디자인을 잘 반영할 수 있을 거 같기 때문
      - 하지만 erossini/BlazorMarkdownEditor 패키지에서는 커스텀된 마크다운
        문법이 있기 때문에 마크다운 텍스트를 MudMarkdown로 html 변환 후
        렌더링하는 것에는 문제가 있음
      - 그래서 일단은 에디터에서 변환한 HTML을 그대로 MudMarkdown 컴포넌트로
        렌더링 하기로 함
      - 그리고 추가적인 css를 오버로딩

--- src/CalmbinoArchive.Web/CalmbinoArchive.Web.Shared/Extensions/ServiceCollectionExtensions.cs
+++ src/CalmbinoArchive.Web/CalmbinoArchive.Web.Shared/Extensions/ServiceCollectionExtensions.cs
@@ -1,5 +1,6 @@
 using CalmbinoArchive.Web.Shared.Services;
 using Microsoft.Extensions.DependencyInjection;
+using MudBlazor;
 using MudBlazor.Services;
 
 namespace CalmbinoArchive.Web.Shared.Extensions;
@@ -10,11 +11,13 @@ public static class ServiceCollectionExtensions
     {
         // Add MudBlazor Services
         services.AddMudServices();
+        services.AddMudMarkdownServices();
 
         // TODO: Find ways to leverage Aspire's Service Discovery
         services.AddScoped<WeatherClientService>();
         services.AddHttpClient<WeatherClientService>(
             static client => client.BaseAddress = new Uri("https://localhost:7245"));
+
 
         return services;
     }
