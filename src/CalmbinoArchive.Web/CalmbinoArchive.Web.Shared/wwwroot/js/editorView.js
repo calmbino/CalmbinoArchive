e4eb2bc5af5926b653f1489314422504997d7ca6
Calmbino <calmbino@gmail.com>
2024-12-08 19:53:52 +0900

    Markdown 렌더링 결과 수정(하이퍼링크)
    - css 적용
    - 클릭시 새탭으로 이동하도록 a태그에 target 설정(자바스크립트 추가)

--- src/CalmbinoArchive.Web/CalmbinoArchive.Web.Shared/wwwroot/js/editorView.js
+++ src/CalmbinoArchive.Web/CalmbinoArchive.Web.Shared/wwwroot/js/editorView.js
@@ -2,3 +2,11 @@ export function callHljs() {
     console.log("CallHljs");
     hljs.highlightAll();
 }
+
+export function changeLinkTarget() {
+    document.querySelectorAll('a').forEach(link => {
+        link.setAttribute('target', '_blank');
+    });
+}
+
+
