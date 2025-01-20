621f4fc0bb3ae67aa8b03600050cfce13f957b93
Calmbino <calmbino@gmail.com>
2025-01-20 22:43:07 +0900

    Blazor Client(WASM)에 Serilog 추가

--- src/CalmbinoArchive.Web/CalmbinoArchive.Web.Client/Program.cs
+++ src/CalmbinoArchive.Web/CalmbinoArchive.Web.Client/Program.cs
@@ -1,7 +1,13 @@
 using CalmbinoArchive.Web.Shared.Extensions;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Serilog;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+Log.Logger = new LoggerConfiguration()
+             .MinimumLevel.Debug()
+             .WriteTo.BrowserConsole()
+             .CreateLogger();
 
 // client(webassembly)에서 service 이름으로 API 호출하기 위한 설정
 // wwwroot/appsettings.json로부터 탐색
