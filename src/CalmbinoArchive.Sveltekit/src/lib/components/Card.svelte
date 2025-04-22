67a83d47769ba34cd42a367611a8c0e2693cfa96
Calmbino <calmbino@gmail.com>
2025-04-23 03:52:42 +0900

    페이지 레이아웃 추가 및 카드 컴포넌트 구현
    - Posts 페이지 (+page.svelte) 생성 및 Card 컴포넌트를 활용한 그리드 레이아웃 적용
    - Card 컴포넌트 (+Card.svelte) 추가
      - 이미지, 제목, 본문 요약, 작성자 및 날짜 표시
      - 호버 효과 및 스타일링 적용
    - app.css에 카드 관련 스타일 유틸리티 추가
    - About 페이지 (+page.svelte) 간단한 설명 텍스트 추가

---
+++ src/CalmbinoArchive.Sveltekit/src/lib/components/Card.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+  const imgSrc =
+    'https://images.unsplash.com/photo-1463171515643-952cee54d42a?q=80&w=450&h=190&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
+</script>
+<a
+    href="posts/1"
+    class="card preset-filled-surface-100-900 border border-surface-200-800 card-hover divide-y divide-surface-200-800 block w-full rounded-xl overflow-hidden"
+>
+  <!-- Header (이미지 비율 고정) -->
+  <header>
+    <img
+        src={imgSrc}
+        alt="banner"
+        class="aspect-[21/9] w-full object-cover grayscale hue-rotate-90"
+    />
+  </header>
+
+  <!-- Article (본문 내용 제한) -->
+  <article class="space-y-2 p-4">
+    <div>
+      <h2 class="text-sm font-semibold text-primary">Announcements</h2>
+      <h3 class="text-lg font-bold">Skeleton is Awesome</h3>
+    </div>
+    <p class="opacity-60 line-clamp-3 text-sm">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam aspernatur provident eveniet eligendi cumque consequatur tempore sint nisi sapiente. Iste beatae laboriosam iure molestias cum expedita architecto itaque quae rem.
+    </p>
+  </article>
+
+  <!-- Footer -->
+  <footer class="flex items-center justify-between gap-4 p-4 text-xs text-muted">
+    <small>By Alex</small>
+    <small>On {new Date().toLocaleDateString()}</small>
+  </footer>
+</a>
---
+++ src/CalmbinoArchive.Sveltekit/src/routes/posts/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+  import Card from "$lib/components/Card.svelte";
+</script>
+
+<div class="grid gap-3 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 max-w-7xl mx-auto">
+  <Card/>
+  <Card/>
+  <Card/>
+  <Card/>
+  <Card/>
+  <Card/>
+  <Card/>
+  <Card/>
+  <Card/>
+</div>
