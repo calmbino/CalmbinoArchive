31e6ffef14651a43086bd4638c2b9d04dd795bfd
Calmbino <calmbino@gmail.com>
2025-04-23 04:12:20 +0900

    컴포넌트 이름 변경
    - Card.svelte 파일명을 PostCard.svelte로 변경
    - 해당 경로 및 import 구문 수정
    - 기존 Card 컴포넌트를 사용하는 부분을 PostCard로 일괄 변경

--- src/CalmbinoArchive.Sveltekit/src/routes/posts/+page.svelte
+++ src/CalmbinoArchive.Sveltekit/src/routes/posts/+page.svelte
@@ -1,15 +1,15 @@
 <script>
-  import Card from "$lib/components/Card.svelte";
+  import PostCard from "$lib/components/PostCard.svelte";
 </script>
 
 <div class="grid gap-3 grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 max-w-7xl mx-auto">
-  <Card/>
-  <Card/>
-  <Card/>
-  <Card/>
-  <Card/>
-  <Card/>
-  <Card/>
-  <Card/>
-  <Card/>
+  <PostCard/>
+  <PostCard/>
+  <PostCard/>
+  <PostCard/>
+  <PostCard/>
+  <PostCard/>
+  <PostCard/>
+  <PostCard/>
+  <PostCard/>
 </div>
